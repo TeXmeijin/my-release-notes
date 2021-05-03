e1faf2bd557b902202a7c7c68740619074ec1c84
TeXmeijin <i8858garden@gmail.com>
2021-05-04 02:49:44 +0900

    add: font

--- components/ogp/OgpContent.tsx
+++ components/ogp/OgpContent.tsx
@@ -1,26 +1,41 @@
 import { Release } from '@/types/release/type'
 import ReactDOM from 'react-dom/server'
 import { ReleaseContent } from '../release/ReleaseContent'
+import React from 'react'
+import path from 'path'
+import fs from 'fs'
+
 const globalStyles = `
   html, body {
     height: 100%;
     display: grid;
     padding: 0;
     margin: 0;
+    font-family: 'M PLUS Rounded 1c', Segoe UI, Helvetica, Arial, sans-serif;
   }
 
-  .markdown-body {
-    font-size: 20px;
+  .markdown-body.markdown-body {
+    font-family: 'M PLUS Rounded 1c', Segoe UI, Helvetica, Arial, sans-serif;
   }
 `
-const OgpContent = ({ release }: OgpProps) => (
+const OgpContent = ({ release, font }: OgpProps) => (
   <html>
     <head>
-      <style>{globalStyles}</style>
       <link
         rel="stylesheet"
         href="https://cdnjs.cloudflare.com/ajax/libs/github-markdown-css/4.0.0/github-markdown.min.css"
       ></link>
+      <style
+        dangerouslySetInnerHTML={{
+          __html: `
+        @font-face {
+          font-family: 'M PLUS Rounded 1c';
+          src: url(data:font/ttf;charset=utf-8;base64,${font}) format('truetype');
+        }
+      `,
+        }}
+      />
+      <style dangerouslySetInnerHTML={{ __html: globalStyles }} />
     </head>
     <body>
       <main
@@ -87,6 +102,12 @@ const OgpContent = ({ release }: OgpProps) => (
 
 export type OgpProps = {
   release: Release
+  font: string
 }
 
-export const GetMarkUp = (props: OgpProps) => ReactDOM.renderToStaticMarkup(<OgpContent {...props} />)
+export const GetMarkUp = (props: OgpProps) => {
+  const fontPath = path.resolve(process.cwd(), './assets/MPLUSRounded1c-Bold.ttf')
+  const font = fs.readFileSync(fontPath, { encoding: 'base64' })
+  const element = React.createElement(OgpContent, { font, release: props.release })
+  return ReactDOM.renderToStaticMarkup(element)
+}
