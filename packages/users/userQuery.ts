de11b9577a2dd95a1e4e9d23048ae8474167dd1c
TeXmeijin <i8858garden@gmail.com>
2021-05-04 16:55:29 +0900

    fix: type

--- packages/users/userQuery.ts
+++ packages/users/userQuery.ts
@@ -6,7 +6,6 @@ export const getAllUsers = async (): Promise<User[]> => {
       userId: 'XXXX',
       name: 'hoge',
       image: 'https://manalink.jp',
-      releasedAt: '2020-08-09T13:49:53.413Z',
     },
   ] as User[]
 }
@@ -16,6 +15,5 @@ export const findUser = async ({ userId }: { userId: User['userId'] }) => {
     userId,
     name: 'hoge',
     image: 'https://manalink.jp',
-    releasedAt: '2020-08-09T13:49:53.413Z',
   }
 }
