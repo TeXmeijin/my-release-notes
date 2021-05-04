62749bf36e1db5f611396810eb2ca86feb802440
TeXmeijin <i8858garden@gmail.com>
2021-05-05 00:58:00 +0900

    add: credit

--- components/shared/MyFooter.tsx
+++ components/shared/MyFooter.tsx
@@ -5,12 +5,21 @@ import { pagesPath } from '../../lib/$path'
 export const MyFooter = () => {
   return (
     <footer className={styles.footer}>
-      <Link href={pagesPath.$url()} passHref>
-        <a className={styles.link}>トップページ</a>
-      </Link>
-      <Link href={pagesPath.releases.$url()} passHref>
-        <a className={styles.link}>リリースノート一覧</a>
-      </Link>
+      <div className={styles.footer__body}>
+        <Link href={pagesPath.$url()} passHref>
+          <a className={styles.link}>トップページ</a>
+        </Link>
+        <Link href={pagesPath.releases.$url()} passHref>
+          <a className={styles.link}>リリースノート一覧</a>
+        </Link>
+      </div>
+      <div className={styles.footer__body}>
+        <Link href="https://lottiefiles.com/29488-note-icon" passHref>
+          <a target="_blank" className={styles.credit}>
+            Animation by snowman on LottieFiles
+          </a>
+        </Link>
+      </div>
     </footer>
   )
 }
